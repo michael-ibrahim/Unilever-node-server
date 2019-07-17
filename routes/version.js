1e2fdcd525df44da3d88896a1a05b52511b4b87d
Michael-Ibrahim-1992 <michael.ibrahim@imperialcreation.com>
2019-07-17 23:16:26 +0200

    v2.2

--- routes/version.js
+++ routes/version.js
@@ -1,7 +1,9 @@
 const router = new (require('restify-router')).Router();
+const version = 2.2;
 
 router.get('/', function (req, res, next) {
-	res.end("v2.1")
+	res.status(200);
+	res.json({version: version})
 });
 
 module.exports = router;
